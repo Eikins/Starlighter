df8fa2dcca65bd5b8e335272ee86f211793d3d45
lauretao08 <taolaurent@gmail.com>
2020-11-18 15:00:02 +0100

    Added basic way to add obstacles in level design
    able to :
    -add unmoving obstacles with a simple prefab
    -add an obstacle that may be launched in a certain direction, with a selectable speed when a trigger plane is touched by the player
    
    Remains to add the damaging aspect

---
+++ Assets/Project/Script/Gameplay/Obstacle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    public bool activable=false;
+
+    [SerializeField]
+    public Vector3 movementDirection = new Vector3(0.0f,0.0f,0.0f);
+
+    [SerializeField]
+    public float speed;
+
+    [SerializeField]
+    public float lifeTimeAfterActivation=10.0f;
+
+    [SerializeField]
+    public int damage = 1;
+
+    private Rigidbody rb;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate()
+    {
+        rb.velocity = movementDirection.normalized * speed;
+        Destroy(gameObject, lifeTimeAfterActivation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //code for damaging player here
+        }
+    }
+}
