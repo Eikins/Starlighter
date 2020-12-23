8a0d7667a6066a1d8e6e4a2b5718bc9fac06347f
lauretao08 <taolaurent@gmail.com>
2020-12-23 17:32:12 +0100

    added asset for stationary obstacles
    buildings sci-fi from asset store

--- Assets/Project/Script/Gameplay/Obstacle.cs
+++ Assets/Project/Script/Gameplay/Obstacle.cs
@@ -48,7 +48,7 @@ public class Obstacle : MonoBehaviour
         if (other.tag == "Player")
         {
             //code for damaging player here
-            Debug.Log("ASTEROID HIT PLAYER");
+            
         }
     }
 }
